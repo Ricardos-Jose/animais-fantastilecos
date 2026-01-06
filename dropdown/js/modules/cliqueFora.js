1999e98406660b5a1a83d60ccf1af7a72966c3cb
Ricardos-Jose <ricardojosegusmaodelima@gmail.com>
2026-01-06 11:00:10 -0300

    Sobre

---
+++ dropdown/js/modules/cliqueFora.js
@@ -0,0 +1,19 @@
+export default function outsideClick(elements, events, callback) {
+  const html = document.documentElement;
+  const outside = 'data-outside';
+  if (!elements.hasAttribute(outside)) {
+    events.forEach((userEvent) => {
+      html.addEventListener(userEvent, handleOutsideClick);
+    });
+    elements.setAttribute(outside, '');
+  }
+  function handleOutsideClick(event) {
+    if (!elements.contains(event.target)) {
+      elements.removeAttribute(outside);
+      events.forEach((userEvent) => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      });
+      callback();
+    }
+  }
+}
---
+++ dropdown/js/script.js
@@ -0,0 +1,17 @@
+// const sectionsAnimais = document.querySelectorAll('.animais-descricao section');
+// console.log(sectionsAnimais);
+
+import initScrollSuave from './modules/scroll-suave.js';
+import initScrollSite from './modules/animacao-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/animacao-tabnav-menu.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropDown from './modules/dropdown.js';
+initScrollSuave();
+initScrollSite();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropDown();
